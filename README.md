1fa2e6bbac8b83c15dac40194c1691fcf5fe0579
Ricardo Castillo <ricardo.castillo@dualbootpartners.com>
2021-04-04 11:04:59 -0300

    Project started

---
+++ README.md
@@ -0,0 +1,3 @@
+# FIGMA
+
+https://www.figma.com/file/Z05xhkA8jMID4kQD3P8bYr/harmonic-composition?node-id=382%3A164
---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+const gulp = require("gulp")
+const sass = require("gulp-sass")
+const watchSass = require("gulp-watch-sass")
+  
+gulp.task("sass:watch", () => watchSass([
+  "./sass/**/*.{scss,css}"
+])
+  .pipe(sass())
+  .pipe(gulp.dest("./public")));
+
+gulp.task('watch', gulp.series('sass:watch'));
